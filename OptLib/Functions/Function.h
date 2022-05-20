2fd3ca9c13d5a7b751e742b16437b39d31b6371a
Danil482 <101705199+Danil482@users.noreply.github.com>
2022-05-20 15:20:26 +0300

    Update Function.h

--- OptLib/Functions/Function.h
+++ OptLib/Functions/Function.h
@@ -143,24 +143,21 @@ namespace OptLib
 			}
 		};
 
-
-
-
 		template <size_t dim>
 		class FuncAlongGradDirection : public FuncInterface::IFuncWithGrad<1>
 		{
 		public:
-			FuncAlongGradDirection(FuncInterface::IFuncWithGrad<dim>* f_pointer, const Point<dim>& x0_) noexcept :
-				x0{ x0_ }, grad0{ f_pointer->grad(x0_) }, f{ *f_pointer }{}
+			FuncAlongGradDirection(const FuncInterface::IFuncWithGrad<dim>* f_pointer,Point<dim>& x0_) noexcept :
+				x0{ x0_ }, grad0{ f_pointer->grad(x0_) }, f{ f_pointer }{}
 
 			virtual double operator () (const Point<1>& gamma) const override
 			{
-				return f(x0 - grad0 * gamma[0]);
+				return f->operator()(x0 - grad0 * gamma[0]);
 			}
 
 			virtual Point<1> grad(const Point<1>& gamma) const override
 			{
-				Point<dim> gr = f.grad(x0 - grad0 * gamma[0]);
+				Point<dim> gr = f->grad(x0 - grad0 * gamma[0]);
 
 				return Point<1>{-dot_product(gr, grad0)};
 			}
@@ -169,7 +166,7 @@ namespace OptLib
 			Point<dim> x0;
 			Point<dim> grad0;
 
-			FuncInterface::IFuncWithGrad<dim>&  f;// function to optimize
+			const FuncInterface::IFuncWithGrad<dim>*  f;// function to optimize
 		};
 
 		template<size_t dimX, size_t dimP>
