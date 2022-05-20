a1a2a37a38a5cac468efd93a0fa90137af4ee9da
Danil482 <101705199+Danil482@users.noreply.github.com>
2022-05-20 13:21:44 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@
 //#include <matplotlibcpp.h>
 
 #ifdef DEBUG_LIB
-	#include "OptLib/TestMethods/testSimplex.h"
-	#include "OptLib/TestMethods/testFunction.h"
-	#include "OptLib/TestMethods/testState.h"
-	#include "OptLib/TestMethods/testOptimizer.h"
-	#include "OptLib/TestMethods/testRegression.h"
+#include "OptLib/TestMethods/testSimplex.h"
+#include "OptLib/TestMethods/testFunction.h"
+#include "OptLib/TestMethods/testState.h"
+#include "OptLib/TestMethods/testOptimizer.h"
+#include "OptLib/TestMethods/testRegression.h"
 #endif // DEBUG_LIB
 
 //namespace plt = matplotlibcpp;
@@ -20,10 +20,10 @@ int main()
 	plt::savefig("minimal.pdf");*/
 
 #ifdef DEBUG_LIB
-	// test Simplex
+	 //test Simplex
 	OptLib::UnitTests::testSimplex::testPointOperations();
 	OptLib::UnitTests::testSimplex::testSimplexVal();
-	
+
 	// test functions
 	OptLib::UnitTests::testFunction::testGradient();
 	OptLib::UnitTests::testFunction::testHessian();
@@ -43,19 +43,21 @@ int main()
 
 	// test optimizers
 	OptLib::UnitTests::testOptimizer::testBicection();
-	OptLib::UnitTests::testOptimizer::testOverallOptimizerWithBicection(); 
+	OptLib::UnitTests::testOptimizer::testOverallOptimizerWithBicection();
 	OptLib::UnitTests::testOptimizer::testDichotomy();
-	OptLib::UnitTests::testOptimizer::testOverallOptimizerWithDichotomy(); 
+	OptLib::UnitTests::testOptimizer::testOverallOptimizerWithDichotomy();
 	OptLib::UnitTests::testOptimizer::testGoldenSection();
 	OptLib::UnitTests::testOptimizer::testOverallOptimizerWithGoldenSection();
 	OptLib::UnitTests::testOptimizer::testGrid();
 	OptLib::UnitTests::testOptimizer::testOverallOptimizerWithGrid();
 	OptLib::UnitTests::testOptimizer::testOverallOptimizerWithNelderMead();
 	OptLib::UnitTests::testOptimizer::testOverallOptimizerWithNewton();
+	OptLib::UnitTests::testOptimizer::testGradientDescent();
+	OptLib::UnitTests::testOptimizer::testOverallOptimizerWithGradientDescent();
 
 	// test regression
 
-	OptLib::UnitTests::testLikelihood::GenerateDataSet<1,1>({1.0}, new OptLib::ConcreteFuncParam::LinearFunc());
+	OptLib::UnitTests::testLikelihood::GenerateDataSet<1, 1>({ 1.0 }, new OptLib::ConcreteFuncParam::LinearFunc());
 	OptLib::UnitTests::testLikelihood::testLikelihoodLinear();
 	OptLib::UnitTests::testLikelihood::testLikelihoodLinearWithGrad();
 	OptLib::UnitTests::testLikelihood::LikelihoodMinimization();
